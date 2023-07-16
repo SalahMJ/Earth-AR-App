2e30cc9b506db5e119f4e10e17b4f2673fad53a7
SalahMJ <salahmajgaonkar2002@gmail.com>
2023-07-16 22:19:37 +0530

    Added ARcore and utilities

--- lib/ar_earth_screen.dart
+++ lib/ar_earth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' as vector64;
 
 class ArEarthScreen extends StatefulWidget {
   const ArEarthScreen({super.key});
@@ -13,7 +15,36 @@ class _ArEarthScreenState extends State<ArEarthScreen> {
 
   augmentedRealityViewCreated(ArCoreController arCoreController){
     augmentedRealityCoreController = arCoreController;
+
+    displayEarthSphere(augmentedRealityCoreController!);
+
   }
+
+  displayEarthSphere(ArCoreController arCoreController) async {
+    final ByteData earthTextureBytes = await rootBundle.load("images/earth_map.jpg");
+
+    final material = ArCoreMaterial(
+      color: Colors.blue,
+      textureBytes: earthTextureBytes.buffer.asUint8List(),
+    );
+
+    final sphere = ArCoreSphere(
+        materials: [
+      material
+    ]
+    );
+
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector64.Vector3(0,0,-1.5)
+    );
+
+    augmentedRealityCoreController?.addArCoreNode(node);
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
